59faadf983f87cde149fd25cf7141593085d7edc
Robert Virkus <robert.virkus@enough.de>
2021-03-03 15:18:35 +0100

    set content-type meta data

--- lib/src/editor_api.dart
+++ lib/src/editor_api.dart
@@ -120,6 +120,7 @@ class EditorApi {
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="content-type" content="text/html;charset="utf-8">
 <style>$styles</style>
 </head>
 <body>$content</body>
